6a9edaa9ad8d583a66aae29d4424cb14f541c5cd
Anran Zhao <nrb6yu@virginia.edu>
2024-11-27 10:49:28 -0500

    Rivanna

---
+++ .ipynb_checkpoints/Header-checkpoint.h
@@ -0,0 +1,14 @@
+//Transparent_BC.h
+#define _USE_MATH_DEFINES
+
+#include <iostream>
+#include <vector>
+#include <complex>
+#include <cmath>
+#include <fstream>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+using namespace std;
+using namespace std::complex_literals;
+
